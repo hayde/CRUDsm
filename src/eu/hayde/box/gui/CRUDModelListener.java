623af581dcdd72dcf855669144e0fdf541c7c814
Administrator <Administrator@bilal.Home>
2015-03-28 22:04:47 +0500

    Formationg of classes

--- src/eu/hayde/box/gui/CRUDModelListener.java
+++ src/eu/hayde/box/gui/CRUDModelListener.java
@@ -4,20 +4,22 @@
 package eu.hayde.box.gui;
 
 /**
- * 
+ *
  * @author can.senturk
  */
 public abstract class CRUDModelListener {
-	
-	public enum CRUD_ACTION_DELETE_OR_CANCEL {
-		CANCEL, DELETE
-	}
-	
-	public enum CRUD_ACTION_AUTOMATED_SAVE {
-		CANCEL, DISCARD, SAVE
-	}
-	
-	public abstract CRUD_ACTION_AUTOMATED_SAVE actionAutomatedSave();
-	public abstract CRUD_ACTION_DELETE_OR_CANCEL actionDeleteOrCancel();
-	
+
+    public enum CRUD_ACTION_DELETE_OR_CANCEL {
+
+        CANCEL, DELETE
+    }
+
+    public enum CRUD_ACTION_AUTOMATED_SAVE {
+
+        CANCEL, DISCARD, SAVE
+    }
+
+    public abstract CRUD_ACTION_AUTOMATED_SAVE actionAutomatedSave();
+
+    public abstract CRUD_ACTION_DELETE_OR_CANCEL actionDeleteOrCancel();
 }
--- src/eu/hayde/box/gui/DefaultCRUDModelListener.java
+++ src/eu/hayde/box/gui/DefaultCRUDModelListener.java
@@ -13,79 +13,71 @@ import javax.swing.UIManager;
  */
 public class DefaultCRUDModelListener extends CRUDModelListener {
 
-	public static class WarningMessages {
-		public static String DELETE_ARE_YOU_SURE = "Sind Sie sicher, dass Sie die Daten löschen wollen?";
-		public static String DELETE_MESSAGE_TITLE = "Löschen";
-		public static String DELETE_BUTTON_TEXT = "Löschen";
+    public static class WarningMessages {
 
-		public static String SAVE_SHOULD_SAVE_IT = "Sollen die eingegebenen Daten gespeichert werden?";
-		public static String SAVE_MESSAGE_TITLE = "Änderungen speichern";
-		public static String SAVE_BUTTON_YES = "Speichern";
-		public static String SAVE_BUTTON_NO = "Nein";
+        public static String DELETE_ARE_YOU_SURE = "Sind Sie sicher, dass Sie die Daten löschen wollen?";
+        public static String DELETE_MESSAGE_TITLE = "Löschen";
+        public static String DELETE_BUTTON_TEXT = "Löschen";
+        public static String SAVE_SHOULD_SAVE_IT = "Sollen die eingegebenen Daten gespeichert werden?";
+        public static String SAVE_MESSAGE_TITLE = "Änderungen speichern";
+        public static String SAVE_BUTTON_YES = "Speichern";
+        public static String SAVE_BUTTON_NO = "Nein";
+        public static String CANCEL_BUTTON_TEXT = "Abbrechen";
+    }
 
-		public static String CANCEL_BUTTON_TEXT = "Abbrechen";
-
-	}
-
-	@Override
-	public CRUD_ACTION_AUTOMATED_SAVE actionAutomatedSave() {
-		        // save first!
+    @Override
+    public CRUD_ACTION_AUTOMATED_SAVE actionAutomatedSave() {
+        // save first!
         CRUD_ACTION_AUTOMATED_SAVE returnValue = CRUD_ACTION_AUTOMATED_SAVE.CANCEL;
 
         //Custom button text
-        Object[] options =
-        {
+        Object[] options = {
             WarningMessages.SAVE_BUTTON_YES, WarningMessages.SAVE_BUTTON_NO, WarningMessages.CANCEL_BUTTON_TEXT
         };
         int selectedValue = JOptionPane.showOptionDialog(null,
-                                                         WarningMessages.SAVE_SHOULD_SAVE_IT,
-                                                         WarningMessages.DELETE_MESSAGE_TITLE,
-                                                         JOptionPane.YES_NO_CANCEL_OPTION,
-                                                         JOptionPane.QUESTION_MESSAGE,
-                                                         null,
-                                                         options,
-                                                         options[2]);
-        if (selectedValue == 0)
-        {
-			returnValue = CRUD_ACTION_AUTOMATED_SAVE.SAVE;
+                WarningMessages.SAVE_SHOULD_SAVE_IT,
+                WarningMessages.DELETE_MESSAGE_TITLE,
+                JOptionPane.YES_NO_CANCEL_OPTION,
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                options,
+                options[2]);
+        if (selectedValue == 0) {
+            returnValue = CRUD_ACTION_AUTOMATED_SAVE.SAVE;
         }
 
-        if (selectedValue == 1)
-        {
-			returnValue = CRUD_ACTION_AUTOMATED_SAVE.DISCARD;
+        if (selectedValue == 1) {
+            returnValue = CRUD_ACTION_AUTOMATED_SAVE.DISCARD;
         }
 
         return returnValue;
-	}
+    }
 
-	@Override
-	public CRUD_ACTION_DELETE_OR_CANCEL actionDeleteOrCancel() {
+    @Override
+    public CRUD_ACTION_DELETE_OR_CANCEL actionDeleteOrCancel() {
         CRUD_ACTION_DELETE_OR_CANCEL returnValue = CRUD_ACTION_DELETE_OR_CANCEL.CANCEL;
 
         String originalValue = (String) UIManager.get("OptionPane.okButtonText");
-        UIManager.put("OptionPane.okButtonText", WarningMessages.DELETE_BUTTON_TEXT );
+        UIManager.put("OptionPane.okButtonText", WarningMessages.DELETE_BUTTON_TEXT);
 
         //Custom button text
-        Object[] options =
-        {
+        Object[] options = {
             WarningMessages.DELETE_BUTTON_TEXT, WarningMessages.CANCEL_BUTTON_TEXT
         };
         int i = JOptionPane.showOptionDialog(null,
-                                                WarningMessages.DELETE_ARE_YOU_SURE,
-                                                WarningMessages.DELETE_MESSAGE_TITLE,
-                                                JOptionPane.OK_CANCEL_OPTION,
-                                                JOptionPane.QUESTION_MESSAGE,
-                                                null,
-                                                options,
-                                                options[1]);
+                WarningMessages.DELETE_ARE_YOU_SURE,
+                WarningMessages.DELETE_MESSAGE_TITLE,
+                JOptionPane.OK_CANCEL_OPTION,
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                options,
+                options[1]);
 
-        if (i == 0)
-        {
+        if (i == 0) {
             returnValue = CRUD_ACTION_DELETE_OR_CANCEL.DELETE;
         }
 
         UIManager.put("OptionPane.okButtonText", originalValue);
         return returnValue;
-	}
-
+    }
 }
